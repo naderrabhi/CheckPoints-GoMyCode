8a18241f6ece7822a68e4a7c0e0eb431f245a1c0
nader rabhi <naderrabhi93@outlook.com>
2022-07-23 18:05:23 +0100

    update checkpoint DOM

--- check-point DOM/script.js
+++ check-point DOM/script.js
@@ -1,41 +1,82 @@
-function tot1 () {
+let heartColor = document.getElementsByClassName("heart")
+        for (const el of heartColor) {
+            el.addEventListener("click", function(){
+                if (el.style.color == "red") {
+                    el.style.color = "black"
+                }else {
+                    el.style.color = "red"
+                }
+            })
+        }
 
-    var price1 = document.getElementById("qty1").value;
-    var pricetot1 = price1 * 22 ;
-    document.getElementById("tot1").value = pricetot1 ;
-}
-function tot2 () {
+        function sumTotal () {
+            let qteV = document.getElementsByClassName("qte");
+            let prixV = document.getElementsByClassName("priceVape");
+            let prixT = document.getElementsByClassName("totalPrice");
+                 for (let i = 0; i < qteV.length; i++) {
+                  let  sumT = parseInt(prixV[i].innerText) * qteV[i].value
+                prixT[i].innerHTML =  sumT ;
+             }
+        }
 
-    var price2 = document.getElementById("qty2").value;
-    var pricetot2 = price2 * 22 ;
-    document.getElementById("tot2").value = pricetot2 ;
-}
+        let btnMinus = document.getElementsByClassName("minus")
 
-function tot () {
-    document.getElementById("tot").value = parseInt(document.getElementById("tot1").value) + parseInt(document.getElementById("tot2").value);
-}
+        for (const el of btnMinus) {
+            el.addEventListener("click" , function(){
+                if (el.nextElementSibling.value == 0){
+                    return;
+                }else {
+                el.nextElementSibling.value--
+                sumTotal ()
+            }
+            })
+           
+            }
+        
+    let btnPlus = document.getElementsByClassName("plus")
 
-function remove1 () {
-    document.getElementById("card1").remove("table");
-}
-function remove2 () {
-    document.getElementById("card2").remove("table");
-}
-function background1() {
-    document.getElementById("h1").style.color = "red";
-}
-function background2() {
-    document.getElementById("h2").style.color = "red";
-}
-function moin1 () {
-    document.getElementById("qty1").value = parseInt(document.getElementById("qty1").value) - 1;
-}
-function moin2 () {
-    document.getElementById("qty2").value = parseInt(document.getElementById("qty2").value) - 1;
-}
-function plus1 () {
-    document.getElementById("qty1").value = parseInt(document.getElementById("qty1").value) + 1;
-}
-function plus2 () {
-    document.getElementById("qty2").value = parseInt(document.getElementById("qty2").value) + 1;
-}
+        for (const el of btnPlus) {
+            el.addEventListener("click" , function(){
+                el.previousElementSibling.value++;
+                sumTotal ()
+            })
+            
+        }
+
+        let btnAdd = document.getElementsByClassName("btnadd");
+        for (let i = 0; i < btnAdd.length; i++) {
+            btnAdd[i].addEventListener("click",function(){
+                
+                let titV = document.getElementsByClassName("titleV")[i].innerHTML;
+                let priV = document.getElementsByClassName("priceVape")[i].innerHTML;
+                let totV = document.getElementsByClassName("totalPrice")[i].innerHTML;
+                let sumt = document.getElementById("price-tot") ;
+                if (totV == "0") {
+                    alert ("Please choose product")
+                }else{
+                    
+                
+        var divtest= document.createElement("div");
+        let nevButt = document.createElement ("button");
+        nevButt.textContent = "remove"
+        nevButt.setAttribute("class", "remove");
+
+        divtest.setAttribute("class", "divT mt-1") ;
+        
+        divtest.innerHTML = `<div class="divt"> ${titV} </div> <div class="divt"> ${priV} </div> <div class="divt"> ${totV} dt </div>`;
+        divtest.appendChild(nevButt)
+        document.getElementById("addcard").appendChild(divtest);
+
+        sumt.innerHTML = parseInt(sumt.innerHTML) + parseInt(totV)
+
+        nevButt.addEventListener("click",function () {
+            divtest.remove()
+            let sumt = document.getElementById("price-tot") ;
+            sumt.innerHTML = parseInt(sumt.innerHTML) - parseInt(totV)
+        })
+    }
+            })
+         
+        }
+
+        
