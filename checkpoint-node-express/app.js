efc8d9dc3b0b82f0bc6e321b1f2b1a575280d30a
nader rabhi <naderrabhi93@outlook.com>
2022-10-02 10:59:52 +0100

    update checkpoint express

--- checkpoint-node-express/app.js
+++ checkpoint-node-express/app.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const fs = require("fs");
+const morgan = require('morgan')
 
 const app = express();
 app.set('view engine', 'ejs');
 
 app.listen(4000);
 
-const date = new Date();
-app.use((req,res,next)=> {
-  if (
-    date.getHours() > 9 &&
-    date.getHours() < 17 &&
-    date.getDay() > 0 &&
-    date.getDay() < 6
-  ) {
-    next();
-  }else {
-    res.send('<h1>Sorry .. This web application is only available during Monday to Friday, from 9 to 17</h1>')
-  }  
-})
+app.use(express.static('public'))
+
+// const date = new Date();
+// app.use((req,res,next)=> {
+//   if (
+//     date.getHours() > 9 &&
+//     date.getHours() < 17 &&
+//     date.getDay() > 0 &&
+//     date.getDay() < 6
+//   ) {
+//     next();
+//   }else {
+//     res.send('<h1>Sorry .. This web application is only available during Monday to Friday, from 9 to 17</h1>')
+//   }  
+// })
 
 app.get("/", (req, res) => {
   res.status(200).render("Homepage.ejs", {title: "Homepage"});
