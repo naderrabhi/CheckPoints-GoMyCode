99ce524c86c17add19cdd2c66b6674c45de355aa
nader rabhi <naderrabhi93@outlook.com>
2022-10-03 16:00:34 +0100

    update express

--- checkpoint-node-express/app.js
+++ checkpoint-node-express/app.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const fs = require("fs");
-const morgan = require('morgan')
+const fs = require("fs");0
 
+//instance of method express
 const app = express();
 app.set('view engine', 'ejs');
 
-app.listen(4000);
+const port = process.env.PORT || 4000
 
 app.use(express.static('public'))
 
-// const date = new Date();
-// app.use((req,res,next)=> {
-//   if (
-//     date.getHours() > 9 &&
-//     date.getHours() < 17 &&
-//     date.getDay() > 0 &&
-//     date.getDay() < 6
-//   ) {
-//     next();
-//   }else {
-//     res.send('<h1>Sorry .. This web application is only available during Monday to Friday, from 9 to 17</h1>')
-//   }  
-// })
+const date = new Date();
+app.use((req,res,next)=> {
+  if (
+    date.getHours() > 9 &&
+    date.getHours() < 17 &&
+    date.getDay() > 0 &&
+    date.getDay() < 6
+  ) {
+    next();
+  }else {
+    res.send('<h1>Sorry .. This web application is only available during Monday to Friday, from 9 to 17</h1>')
+  }  
+})
 
 app.get("/", (req, res) => {
   res.status(200).render("Homepage.ejs", {title: "Homepage"});
@@ -35,3 +35,5 @@ app.get("/contact-us", (req, res) => {
 app.use((req, res) => {
   res.status(404).render("404.ejs",{title: "404"});
 });
+
+app.listen(port);
